774c8d6cbf8a3e1642ca7b12d1cde676ebe263fb
Dushyant Goyal <goyaldushyant075@gmail.com>
2026-03-19 22:16:20 +0530

    updated the main server file

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 require("dotenv").config()
 let express = require("express")
 const ConnectDB = require("./configs/db")
+const schoolRouter = require("./routes/schoolRoutes")
 let app = express()
 let PORT = process.env.PORT || 3000
 app.use(express.json())
@@ -10,7 +11,7 @@ ConnectDB()
 app.get("/", (req, res)=>{
     res.send("Welcome to Educase India")
 })
-
+app.use("/school", schoolRouter)
 app.listen(PORT, ()=>{
     console.log(`API is running on Port ${PORT}`)
 })
